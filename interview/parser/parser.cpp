33991fe0d72b16bdd9d81bff6f9241eed5ddd430
Joshua Liew <joshualiew93@gmail.com>
2021-07-07 00:42:35 -0500

    fixed Drive, google comma bug

--- interview/parser/parser.cpp
+++ interview/parser/parser.cpp
@@ -7,45 +7,46 @@ top 3 most visited sites
 #include <fstream>
 #include <unordered_map>
 using namespace std;
+
 vector<string> getTop3Visited(string filename) {
     vector<string> ans, row;
     unordered_map<string, float> hm;
     ifstream myFile(filename);
     if(!myFile.is_open()) throw std::runtime_error("Could not open file");
-    string line, val;
-    
+    string line, val, sitename;
+    bool foundCommaInSite = false;
     while(getline(myFile, line)) {
         stringstream ss(line);
-        size_t found = line.find("\"");
-        if (found != string::npos) {
-            cout << "";
-        }
-        else {
-            while (getline(ss, line, ',')) {
+
+        while (getline(ss, line, ',')) {
+            if (line.find("\"") != string::npos) {
+                string cur;
+                getline(ss, cur, '\"');
+                sitename = line +cur;
+                row.push_back(line);
+                foundCommaInSite = true;
+                continue;
+            }
+            if(line != "") {
                 row.push_back(line);
             }
-            string sitename = row[2];
-            if(hm.find(sitename) == hm.end()) {
-                hm[sitename] = 1;
-            }
-            else {
-                hm[sitename]++;
-            }
-            row.clear();
-            
         }
+
+        sitename = sitename == "" ? row[2] : sitename;
+        hm[sitename] = hm.find(sitename) == hm.end() ? 1 : hm[sitename]+1;
+
+        row.clear();
+        sitename = "";
     }
-    for(auto const&[key, val] : hm) {
-        cout << key << ":" << to_string(val) << endl;
+    for (auto const& [key, val] : hm) {
+        cout << key << ':' << val << endl;
     }
     return ans;
 }
 
 
-/*
-total input and output
-*/
 
 int main() {
     getTop3Visited("screen_share_interview_data.csv");
+    return 0;
 }
